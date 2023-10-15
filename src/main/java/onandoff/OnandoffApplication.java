b693781f5eac0dc30f27ce8ed007c1ce05de5fbc
iamjooon2 <iamjooon2@gmail.com>
2023-10-15 21:46:44 +0900

    initial commit

---
+++ src/main/java/onandoff/OnandoffApplication.java
@@ -0,0 +1,13 @@
+package onandoff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnandoffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnandoffApplication.class, args);
+	}
+
+}
